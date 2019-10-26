a2cb658277b315d4638bd11af11a35e434447f4f
unknown <skopin.n@gmail.com>
2019-10-26 20:03:02 +0200

    responsiveness for media sized screens

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,3 +8,21 @@ toggler.addEventListener('click', () => {
   toggler.classList.toggle('open')
   topNav.classList.toggle('open')
 })
+
+//scroll to top
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    buttonUp.style.display = 'block'
+  } else {
+    buttonUp.style.display = 'none'
+  }
+}
+
+const buttonUp = document.querySelector('.button-up')
+
+buttonUp.addEventListener('click', e => {
+  e.preventDefault()
+
+  document.body.scrollTo({ top: 0, behavior: 'smooth' })
+  document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
+})
