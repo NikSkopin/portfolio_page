5d20ef14da9b53ba25e3930f91fd48baed0c42cb
unknown <skopin.n@gmail.com>
2019-10-27 09:56:21 +0100

    full responsive and add animate on scroll effects

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,37 @@
-// navigation
-console.log('hello')
-
 const toggler = document.querySelector('.menu-toggler')
 const topNav = document.querySelector('.top-nav')
+const navLinks = document.querySelectorAll('.nav-list-link')
+const buttonUp = document.querySelector('.button-up')
 
-toggler.addEventListener('click', () => {
-  toggler.classList.toggle('open')
-  topNav.classList.toggle('open')
-})
+// open and close navigation
+function toggleNav(e) {
+  if (e.target === toggler || e.target.classList.contains('bar')) {
+    toggler.classList.toggle('open')
+    topNav.classList.toggle('open')
+  } else {
+    toggler.classList.remove('open')
+    topNav.classList.remove('open')
+  }
+}
 
-//scroll to top
+//smooth scroll
+function goScroll(e) {
+  e.preventDefault()
+  if (
+    e.target.classList.contains('button-up') ||
+    e.target.classList.contains('fa-chevron-up')
+  ) {
+    document.body.scrollTo({ top: 0, behavior: 'smooth' })
+    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
+    toggleNav(e)
+  } else {
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+}
+
+//disblay up button when scroll from top
 window.onscroll = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     buttonUp.style.display = 'block'
@@ -18,11 +40,12 @@ window.onscroll = () => {
   }
 }
 
-const buttonUp = document.querySelector('.button-up')
+toggler.addEventListener('click', toggleNav)
 
-buttonUp.addEventListener('click', e => {
-  e.preventDefault()
+topNav.addEventListener('click', toggleNav)
 
-  document.body.scrollTo({ top: 0, behavior: 'smooth' })
-  document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
+navLinks.forEach(link => {
+  link.addEventListener('click', goScroll)
 })
+
+buttonUp.addEventListener('click', goScroll)
